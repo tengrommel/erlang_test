3b9894d562da38e2f5376cef042e19c0828410e6
tengrommel <wwwsousoucom@126.com>
2017-12-10 22:35:54 +0800

    factory

--- src/easy.erl
+++ src/easy.erl
@@ -10,7 +10,12 @@
 -author("lu").
 
 %% API
--export([add/2]).
+-export([add/2, factorial/1]).
 
 add(X, Y) -> %% Head
   X + Y.     %% Body
+
+factorial(N) when N > 0 ->
+  N * factorial(N -1);
+factorial(0) ->
+  1.
