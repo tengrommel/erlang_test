640b1d22c7672ec8229e6e86f982a07670bceeb4
tengrommel <wwwsousoucom@126.com>
2017-12-10 23:02:04 +0800

    元组

--- src/easy.erl
+++ src/easy.erl
@@ -10,7 +10,7 @@
 -author("lu").
 
 %% API
--export([add/2, factorial/1]).
+-export([add/2, factorial/1, area/3, area/1]).
 
 add(X, Y) -> %% Head
   X + Y.     %% Body
@@ -19,3 +19,19 @@ factorial(N) when N > 0 ->
   N * factorial(N -1);
 factorial(0) ->
   1.
+
+area(Type, N, M) ->
+  case Type of
+    square -> N * N;
+    circle -> math:pi() * N * N;
+    triangle -> 0.5 * N * M
+end.
+
+area({square, N}) ->
+  N * N;
+area({circle, R}) ->
+  math:pi()*R*R;
+area({triangle, B, H}) ->
+  0.5*B*H;
+area(_)->
+  {error, invalidObject}.
